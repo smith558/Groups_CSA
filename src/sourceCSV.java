73ce9d14c40e1f6f61d43f6a12b5f38862147af5
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-15 10:10:58 +0100

    File Structure

--- src/sourceCSV.java
+++ src/sourceCSV.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class sourceCSV {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("src\\sheet.csv"));
+        Scanner scanner = new Scanner(new File("src\\database\\sheet.csv"));
         scanner.useDelimiter(",");
         while (scanner.hasNext()) {
             System.out.print(scanner.next() + "|");

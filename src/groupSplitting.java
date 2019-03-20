6ee6e3f4d7df4dbd74cd4597494fe8768bd3ab4a
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-20 13:24:15 +0100

    method 'groupByGender'
    not yet finished

--- src/groupSplitting.java
+++ src/groupSplitting.java
@@ -4,9 +4,12 @@ public class groupSplitting {
     public void groupByGender(ArrayList<Student> srcArr, int groupSize) {
         //  have a look at this, proper division
         int numOfGroups = srcArr.size() / groupSize;
+        //  iterating through source
         for (int i = 0; i < srcArr.size(); i++) {
             Student nowStud = srcArr.get(i);
-            if (nowStud.ge)
+            if (nowStud.getGender() == 'f') {
+
+            }
         }
     }
 

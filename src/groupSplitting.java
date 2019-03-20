4d5dfc83e1ebd8c170d835861400d9e9de18b005
jkuceraa <kucerajiri@outlook.com>
2019-03-20 12:27:38 +0100

    group splitting changed

---
+++ src/groupSplitting.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class groupSplitting {
+    void groupByGender(ArrayList<Student> srcArray, int nrInGroup){
+        ArrayList<Student> result = new ArrayList<>();
+
+    }
+}

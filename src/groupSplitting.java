3abc77e9f35f8a7d5c43a6a0563a3dc7efcc8337
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-20 12:47:20 +0100

    Up

--- src/groupSplitting.java
+++ src/groupSplitting.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 
 public class groupSplitting {
-    void groupByGender(ArrayList<Student> srcArray, int nrInGroup) {
-        int num = nrInGroup;
-        for (int i = 0; i < 6; i++) {
-            System.out.println();
-        }
+    public void groupByGender(ArrayList<Student> srcArray, int nrInGroup) {
+
     }
 
     void groupByHall(ArrayList<Student> srcArray, int nrInGroup) {

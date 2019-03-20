a522e3cf1421975fb43367c1ba4b1f62bd9c1ff0
jkuceraa <kucerajiri@outlook.com>
2019-03-20 12:44:04 +0100

    creating a method

--- src/groupSplitting.java
+++ src/groupSplitting.java
@@ -4,4 +4,12 @@ public class groupSplitting {
     void groupByGender(ArrayList<Student> srcArray, int nrInGroup) {
 
     }
+
+    void groupByHall(ArrayList<Student> srcArray, int nrInGroup) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

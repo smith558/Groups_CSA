f3d24cad087d0f682db1f32d5012324f8220baac
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-20 13:12:12 +0100

    Up

--- src/groupSplitting.java
+++ src/groupSplitting.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 
 public class groupSplitting {
-    public void groupByGender(ArrayList<Student> srcArray, int nrInGroup) {
-
+    public void groupByGender(ArrayList<Student> srcArr, int groupSize) {
+        //  have a look at this, proper division
+        int numOfGroups = srcArr.size() / groupSize;
+        for (int i = 0; i < srcArr.size(); i++) {
+            Student nowStud = srcArr.get(i);
+            if (nowStud.ge)
+        }
     }
 
     void groupByHall(ArrayList<Student> srcArray, int nrInGroup) {

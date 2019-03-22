c8d0f892018c4589403d59c02b405a0de285a73c
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-22 09:53:37 +0100

    Up

--- src/GroupSplitting.java
+++ src/GroupSplitting.java
@@ -1,7 +1,7 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
-public class groupSplitting {
+public class GroupSplitting {
     public static int groupByGender(ArrayList<Student> srcArr, int groupSize) {
         //  have a look at this, proper division
         int numOfGroups = srcArr.size() / groupSize;
@@ -31,7 +31,7 @@ public class groupSplitting {
         SourceCSV source = new SourceCSV("src\\database\\sheet.csv");
         System.out.println("females " + source.numOfFemales);
         System.out.println("males " + source.numOfMales);
-        System.out.println("num of groups" + groupSplitting.groupByGender(source.getSourceArr(), 8));
+        System.out.println("num of groups" + GroupSplitting.groupByGender(source.getSourceArr(), 8));
         System.out.println(source.getSourceArr());
         for (Student student : source.getSourceArr()) {
             System.out.println(student);

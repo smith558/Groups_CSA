6705d911d515c5b128cc23dac5c77f0c021d9112
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-25 10:21:38 +0100

    groupByGender repaired
    repair after broken merge

--- src/GroupSplitting.java
+++ src/GroupSplitting.java
@@ -1,17 +1,26 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
-public class groupSplitting {
-    public void groupByGender(ArrayList<Student> srcArr, int groupSize) {
+public class GroupSplitting {
+    public static int groupByGender(ArrayList<Student> srcArr, int groupSize) {
         //  have a look at this, proper division
         int numOfGroups = srcArr.size() / groupSize;
-        //  iterating through source
-        for (int i = 0; i < srcArr.size(); i++) {
-            Student nowStud = srcArr.get(i);
-            if (nowStud.getGender() == 'f') {
 
+        //  iterating through source
+        int gB = 1, gG = 1;
+        for (Student student : srcArr) {
+            student.setGroupIdentifier((byte) 0);
+            if (student.getGender() == 'f') {
+                if (gG > numOfGroups) gG = 1;
+                student.setGroupIdentifier((byte) gG);
+                gG++;
+            } else {
+                if (gB > numOfGroups) gB = 1;
+                student.setGroupIdentifier((byte) gB);
+                gB++;
             }
         }
+        return numOfGroups;
     }
 
     public static void groupByHall(ArrayList<Student> srcArray, int nrInGroup) {
@@ -113,8 +122,13 @@ public class groupSplitting {
 
     public static void main(String[] args) throws FileNotFoundException {
         SourceCSV source = new SourceCSV("src\\database\\sheet.csv");
-        groupByHall(source.sourceArr, 6);
-        System.out.println(source.sourceArr);
-
+        System.out.println("females " + source.numOfFemales);
+        System.out.println("males " + source.numOfMales);
+        System.out.println("num of groups" + GroupSplitting.groupByGender(source.getSourceArr(), 8));
+        System.out.println(source.getSourceArr());
+        for (Student student : source.getSourceArr()) {
+            System.out.println(student);
+            System.out.println();
+        }
     }
 }

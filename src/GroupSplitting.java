584608e8fd49ed8c7a18cbd957c002882a9776f2
Stanley Modrak <stanleysmith@tutanota.de>
2019-03-29 09:31:59 +0100

    documentation

--- src/GroupSplitting.java
+++ src/GroupSplitting.java
@@ -2,21 +2,15 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ *
+ */
 public class GroupSplitting {
     /**
-     * Returns an Image object that can then be painted on the screen.
-     * The url argument must specify an absolute {@link URL}. The name
-     * argument is a specifier that is relative to the url argument.
-     * <p>
-     * This method always returns immediately, whether or not the
-     * image exists. When this applet attempts to draw the image on
-     * the screen, the data will be loaded. The graphics primitives
-     * that draw the image will incrementally paint on the screen.
      *
-     * @param url  an absolute URL giving the base location of the image
-     * @param name the location of the image, relative to the url argument
-     * @return the image at the specified URL
-     * @see Image
+     * @param srcArr
+     * @param groupSize
+     * @return
      */
     public static int groupByGender(ArrayList<Student> srcArr, int groupSize) {
         //  shuffling the srcArr to ensure 'randomness'
